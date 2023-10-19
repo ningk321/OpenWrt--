a655db01ad5674c87583fccbd0e37aa71365ad63
TIAmo <130211313+ysx88@users.noreply.github.com>
2023-10-19 22:33:02 +0800

    -

--- r2s/diy-part2.sh
+++ r2s/diy-part2.sh
@@ -4,9 +4,8 @@
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.1.2/g' package/base-files/files/bin/config_generate
 
-# Themes 主题
+# 添加额外插件
 rm -rf feeds/luci/themes/luci-theme-argon
-rm -rf feeds/luci/applications/luci-app-argon-config
 git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
 git clone -b 18.06 https://github.com/jerrykuku/luci-app-argon-config.git package/luci-app-argon-config
 git clone --depth=1 https://github.com/fw876/helloworld package/luci-app-ssr-plus
