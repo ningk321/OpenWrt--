dbedad5a2f7c292a200c8cfa724a4d53d4295e8d
TIAmo <130211313+ysx88@users.noreply.github.com>
2023-10-31 02:24:39 +0800

    -

--- armv8/diy-part1.sh
+++ armv8/diy-part1.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 
+
 # Add a feed source
 # sed -i '$a src-git helloworld https://github.com/fw876/helloworld' feeds.conf.default
 # sed -i '$a src-git PWpackages https://github.com/xiaorouji/openwrt-passwall.git;packages' feeds.conf.default
