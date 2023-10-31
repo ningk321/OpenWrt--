4da13dd0be60559059914bf9d7ab8700f7e7663a
TIAmo <130211313+ysx88@users.noreply.github.com>
2023-11-01 00:00:48 +0800

    -

--- Phicomm-N1/diy-part1.sh
+++ Phicomm-N1/diy-part1.sh
@@ -4,4 +4,4 @@
 # Uncomment a feed source
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
-sed -i '$a src-git pkgs https://github.com/ysx88/openwrt-packages' feeds.conf.default
+sed -i '$a src-git Jejz https://github.com/Jejz168/openwrt-packages' feeds.conf.default
