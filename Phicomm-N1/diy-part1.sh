160f2e75a79b5a18bd1afe5b0c671b224b6db588
TIAmo <130211313+ysx88@users.noreply.github.com>
2023-11-01 04:45:50 +0800

    -

--- Phicomm-N1/diy-part1.sh
+++ Phicomm-N1/diy-part1.sh
@@ -4,4 +4,4 @@
 # Uncomment a feed source
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
-sed -i '$a src-git silm https://github.com/ysx88/silm-pkgs' feeds.conf.default
+sed -i '$a src-git slim https://github.com/ysx88/slim-pkgs' feeds.conf.default
