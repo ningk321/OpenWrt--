f620cdc01d92b3b3e8dd3818e24ac0f2d9f5f26c
TIAmo <130211313+ysx88@users.noreply.github.com>
2023-11-01 05:50:51 +0800

    -

--- Phicomm-N1/diy-part2.sh
+++ Phicomm-N1/diy-part2.sh
@@ -22,30 +22,8 @@ cp -f $GITHUB_WORKSPACE/diy/bg1.jpg package/luci-theme-argon/htdocs/luci-static/
 # replace banner
 cp -f $GITHUB_WORKSPACE/diy/banner package/base-files/files/etc/banner
 
-# alist
-#git clone https://github.com/sbwml/luci-app-alist package/alist
-
-# vssr
-#git clone https://github.com/jerrykuku/lua-maxminddb.git package/lua-maxminddb
-#git clone https://github.com/jerrykuku/luci-app-vssr.git package/luci-app-vssr
-
-# passwall
-#svn co https://github.com/xiaorouji/openwrt-passwall/trunk/luci-app-passwall package/luci-app-passwall
-
-# adguardhome
-#svn co https://github.com/xiangfeidexiaohuo/openwrt-packages/trunk/luci-app-adguardhome package/luci-app-adguardhome
-
-# 晶晨宝盒
-#svn co https://github.com/ysxhub888/luci-app-amlogic/trunk/luci-app-amlogic package/luci-app-amlogic
-
-# 阿里云盘webdav
-#rm -rf feeds/luci/applications/luci-app-aliyundrive-webdav
-#rm -rf feeds/packages/multimedia/aliyundrive-webdav
-#svn co https://github.com/messense/aliyundrive-webdav/trunk/openwrt/luci-app-aliyundrive-webdav package/luci-app-aliyundrive-webdav
-#svn co https://github.com/messense/aliyundrive-webdav/trunk/openwrt/aliyundrive-webdav package/aliyundrive-webdav
-
 # 科学上网插件依赖
-#svn co https://github.com/vernesong/OpenClash/trunk/luci-app-openclash package/luci-app-openclash
+svn co https://github.com/vernesong/OpenClash/trunk/luci-app-openclash package/luci-app-openclash
 # 编译 po2lmo (如果有po2lmo可跳过)
 pushd package/luci-app-openclash/tools/po2lmo
 make && sudo make install
