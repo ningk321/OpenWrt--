de294ab7b6ed3991e9f6dc0370485c232669be30
TIAmo <130211313+ysx88@users.noreply.github.com>
2023-11-01 04:45:11 +0800

    -

--- Phicomm-N1/diy-part1.sh
+++ Phicomm-N1/diy-part1.sh
@@ -4,4 +4,4 @@
 # Uncomment a feed source
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
-sed -i '$a src-git ysxhub https://github.com/ysxhub888/openwrt-packages' feeds.conf.default
+sed -i '$a src-git silm https://github.com/ysx88/silm-pkgs' feeds.conf.default
